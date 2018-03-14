c666a1cc229f213ef0041ff59cdb15ed1cce3a02
Pedro S. Moreira <pedsmoreira@gmail.com>
2018-03-14 13:09:48 -0300

    Rename touchOnMount to showErrors, add Errors and Submit

--- packages/mobx-form-for/src/index.js
+++ packages/mobx-form-for/src/index.js
@@ -3,5 +3,8 @@
 import Field from './Field';
 import FieldGroup from './FieldGroup';
 import Form from './Form';
+import Errors from './Errors';
+import Submit from './Submit';
 
-export { Field, FieldGroup, Form };
+export { Field, FieldGroup, Form, Errors, Submit };
+export type { ComponentProps } from 'form-for';
